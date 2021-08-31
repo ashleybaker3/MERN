7b66f4d33f625ace42ec6a63203bbb7d09c3fad8
Ashley Baker <bakerashley56@gmail.com>
2021-08-31 15:33:33 -0700

    jsx

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title text-primary">Hello Dojo!</h1>
+      <h3 className="Subheading text-decoration-none">Things I need to do:</h3>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Learn React</li>
+        <li className="list-group-item">Climb Mt. Everest</li>
+        <li className="list-group-item">Run a marathon</li>
+        <li className="list-group-item">Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
