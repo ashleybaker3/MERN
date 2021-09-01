9aaf3891aee41c4157256384bbc2940326b11093
Ashley Baker <bakerashley56@gmail.com>
2021-09-01 13:53:52 -0700

    prop it

---
+++ React/prop-it-up/src/Components/propIt.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {firstName, lastName, age, hairColor} = this.props;
+        return (
+            <div>
+                <h1>{ lastName }, { firstName }</h1>
+                <h3>Age: {age}</h3>
+                <h3>Hair Color : {hairColor}</h3>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
