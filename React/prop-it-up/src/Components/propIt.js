5bc62383aef0756b37a37d87fe849515217bdce2
Ashley Baker <bakerashley56@gmail.com>
2021-09-02 13:48:13 -0700

    prop part 2

--- React/prop-it-up/src/Components/propIt.js
+++ React/prop-it-up/src/Components/propIt.js
@@ -3,14 +3,24 @@ import React, { Component } from "react";
 class PersonCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            age: 0
+        };
     }
+
+    birthday = (event) => {
+        this.setState({age: this.state.age + 1});
+    }
+
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
+        const yearsOld = this.state.age;
         return (
             <div>
                 <h1>{ lastName }, { firstName }</h1>
-                <h3>Age: {age}</h3>
+                <h3>Age: {yearsOld}</h3>
                 <h3>Hair Color : {hairColor}</h3>
+                <button onClick={(event) => {this.birthday(event)}}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
